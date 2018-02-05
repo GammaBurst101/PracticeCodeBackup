7e2dc5a80912e1bd4679b9e54066750eeae84317
Prasoon <prasoonpandey06@gmail.com>
2018-02-05 19:35:23 +0530

    Make a working clock

---
+++ software/Clock2.java
@@ -0,0 +1,53 @@
+package software;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Calendar;
+
+public class Clock2 
+{
+    JFrame frame;
+    JTextField textfield;
+    JPanel panel;
+    
+    public Clock2()
+    {
+        //Setting JFrame
+        frame = new JFrame ("Clock");
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(true);
+        frame.setLocationRelativeTo(null);
+        
+        //initialisations
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        
+        textfield = new JTextField(10);
+        textfield.setEditable(false);
+        textfield.setFont(new Font("Arial", Font.PLAIN, 48));
+        
+        //Add components
+        panel.add(textfield);
+        frame.add(panel);
+        
+        Timer t = new Timer(1000,new Listener());
+        t.start();
+    }
+    
+    class Listener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            Calendar cal = Calendar.getInstance();
+            
+            int hour = cal.get(Calendar.HOUR_OF_DAY);
+            int min = cal.get(Calendar.MINUTE);
+            int sec = cal.get(Calendar.SECOND);
+            
+            textfield.setText(hour+":"+min+":"+sec);
+        }
+    }
+}
---
+++ software/Clock2Exe.java
@@ -0,0 +1,13 @@
+package software;
+class Clock2Exe
+{
+    public static void main (String args[])
+    {
+        new Clock2Exe();
+    }
+    
+    Clock2Exe()
+    {
+        new Clock2();
+    }
+}
